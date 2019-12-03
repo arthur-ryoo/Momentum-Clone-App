d27075a563d663ebb5bcc14727f8176bb535a046
arthur-ryoo <jene0523@gmail.com>
2019-12-03 00:14:31 -0800

    added to do list and weather API

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
-// Clock
+// Weather API
 
+$('form').on('submit', event => {
+  event.preventDefault();
+  const userInput = $('input[type = "text"]').val();
+  console.log(userInput);
+  const promise = $.ajax({
+    url:
+      'http://api.openweathermap.org/data/2.5/weather?appid=b9f45391760c4343ac18d9fe52f90d80&q=' +
+      userInput
+  });
+  promise.then(
+    data => {
+      $('#temp').html(data.main.temp - 273.15);
+      console.log(data.main.temp);
+      $('#icon').empty();
+      $('#icon').prepend(
+        '<img id="image" src=http://openweathermap.org/img/wn/' +
+          data.weather[0].icon +
+          '@2x.png>'
+      );
+    },
+    error => {
+      console.log('bad request: ', error);
+    }
+  );
+});
+
+// Clock
 const clockContainer = document.querySelector('.clock');
 const clockTitle = document.querySelector('.clock-title');
 
@@ -14,9 +41,43 @@ function showTime() {
   }:${second < 10 ? `0${second}` : second}`;
 }
 
-function init() {
-  showTime();
-  setInterval(showTime, 1000);
+// To do list
+const toDoListContainer = document.querySelector('.todolist');
+const toDoListInput = document.querySelector('.todolist-input');
+const toDoListItem = document.querySelector('.todolist-item');
+const toDoListLocalStorage = 'toDo';
+
+function addToDoList(text) {
+  const liList = document.createElement('li');
+  const deleteButton = document.createElement('button');
+  deleteButton.innerHTML = 'Delete';
+  const span = document.createElement('span');
+  span.innerText = text;
+  liList.appendChild(span);
+  liList.appendChild(deleteButton);
+  toDoListItem.appendChild(liList);
+
+  console.log(text);
 }
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const inputValue = toDoListInput.value;
+  addToDoList(inputValue);
+  toDoListInput.value = '';
+}
+
+function loadToDoList() {
+  const toDo = localStorage.getItem(toDoListLocalStorage);
+}
+
+function init() {
+  // Clock
+  showTime();
+  setInterval(showTime, 1000);
+
+  // To do list
+  loadToDoList();
+  toDoListContainer.addEventListener('submit', handleSubmit);
+}
 init();
